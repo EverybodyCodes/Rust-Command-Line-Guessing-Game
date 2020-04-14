da9bb5fbf57e0a6fd12c4474ee2e8d0b1d8a7c84
JimLynchCodes <mrdotjim@gmail.com>
2020-04-14 14:05:57 -0400

    reading args

--- guessing_game/target/debug/guessing_game.d
+++ guessing_game/target/debug/guessing_game.d
@@ -1,1 +1,1 @@
-/Users/jlynch/Git-Projects/Rust-Command-Line-Guessing-Game/guessing_game/target/debug/guessing_game: /Users/jlynch/Git-Projects/Rust-Command-Line-Guessing-Game/guessing_game/src/main.rs
+/Users/jlynch/Git-Forks/Rust-Command-Line-Guessing-Game/guessing_game/target/debug/guessing_game: /Users/jlynch/Git-Forks/Rust-Command-Line-Guessing-Game/guessing_game/src/main.rs
